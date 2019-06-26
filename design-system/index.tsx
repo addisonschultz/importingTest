e7e0caf62c940336c181f957c3541375e5e3abc1
Addison <addison@framer.com>
2019-06-26 14:04:38 +0200

    added a NEW component har har

--- design-system/index.tsx
+++ design-system/index.tsx
@@ -4,3 +4,4 @@
 export { Example, Props as ExampleProps } from "./components/Example";
 export { Button, Props as ButtonProps } from "./components/Button";
 export { List, Props as ListProps } from "./components/List";
+export { New, Props as NewProps } from "./components/New";
