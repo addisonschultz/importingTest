11f5e0829673e2b97ebbc627e8a55fbabfbacbd1
Addison <addison@framer.com>
2019-06-26 12:21:37 +0200

    update

--- ComponentImporter/src/babel-generator.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "@babel/generator" {
-    export default function(ast: any): GenerateResult
-    export interface GenerateResult {
-        code: string
-    }
-}
--- ComponentImporter/src/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./babel"
-export * from "./analyze"
-export * from "./convert"
-export * from "./generate"
-export * from "./types"
-export * from "./typescript"
-export * from "./utils"
