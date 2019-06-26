4bba01baab13ed5470524ab8595941bfc55f288f
Addison <addison@framer.com>
2019-06-26 16:47:18 +0200

    resetting kit

--- design-system/index.tsx
+++ design-system/index.tsx
@@ -2,4 +2,3 @@
 // example: export { Button, Props as ButtonProps } from "./components/Button"
 
 export { Example, Props as ExampleProps } from "./components/Example";
-export { Example2, Props as Example2Props } from "./components/ex2";
