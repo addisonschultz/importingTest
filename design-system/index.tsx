7beebbf708c43c663210e69ef63abe3408607b89
Addison <addison@framer.com>
2019-06-26 14:11:07 +0200

    bringing project back to start

--- design-system/index.tsx
+++ design-system/index.tsx
@@ -2,6 +2,3 @@
 // example: export { Button, Props as ButtonProps } from "./components/Button"
 
 export { Example, Props as ExampleProps } from "./components/Example";
-export { Button, Props as ButtonProps } from "./components/Button";
-export { List, Props as ListProps } from "./components/List";
-export { New, Props as NewProps } from "./components/New";
