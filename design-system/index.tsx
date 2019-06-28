06335e53ef07bbd7712728a5370fd507b02b3a18
Addison <addison@framer.com>
2019-06-28 15:17:25 +0200

    added list component

--- design-system/index.tsx
+++ design-system/index.tsx
@@ -3,3 +3,4 @@
 
 export { Example, Props as ExampleProps } from "./components/Example";
 export { Button, Props as ButtonProps } from "./components/Button";
+export { List, Props as ListProps } from "./components/List";
