908439b8375538fbdc7d82021b9853750de39c1b
Addison <addison@framer.com>
2019-06-28 15:48:45 +0200

    refresh

--- design-system/index.tsx
+++ design-system/index.tsx
@@ -2,5 +2,3 @@
 // example: export { Button, Props as ButtonProps } from "./components/Button"
 
 export { Example, Props as ExampleProps } from "./components/Example";
-export { Button, Props as ButtonProps } from "./components/Button";
-export { List, Props as ListProps } from "./components/List";
