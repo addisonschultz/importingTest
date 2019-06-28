2c7ae7b5b06b7dc0d4ca5440bece63080e319533
Addison <addison@framer.com>
2019-06-28 11:55:34 +0200

    added new component

--- design-system/index.tsx
+++ design-system/index.tsx
@@ -2,3 +2,4 @@
 // example: export { Button, Props as ButtonProps } from "./components/Button"
 
 export { Example, Props as ExampleProps } from "./components/Example";
+export { Button, Props as ButtonProps } from "./components/Button";
